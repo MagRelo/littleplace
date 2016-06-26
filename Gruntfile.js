7fea84b751aa6424d3b006c01382c84ec89a305c
Matt Lovan <magrelo404@gmail.com>
2016-06-25 20:33:03 -0400

    add dockerfile to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -395,6 +395,7 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             'package.json',
+            'Dockerfile',
             '<%= yeoman.server %>/**/*'
           ]
         }]
